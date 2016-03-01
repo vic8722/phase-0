75df8bc712db416255bd5465a24b4d334aa225c4
vic8722 <victoria.trash@gmail.com>
2016-03-01 15:40:57 -0800

    Create file with definitions of add, subtract, multiply and divide.

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ #your code here
+ return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ #your code here
+ return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ #your code here
+ return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ #your code here
+ return num_1.to_f / num_2.to_f
+end
+
